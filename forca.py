7d5c363dca35ef97d60f0aaa32aae801baaffef7
Gilberto <gilberto.braga0021@gmail.com>
2023-10-14 13:22:55 -0300

    Mensagem de testes.

---
+++ forca.py
@@ -0,0 +1,38 @@
+# Jogo da Forca -- adaptado de https://youtu.be/_A_2aTFHzJ0?si=N2XN-35MLTjvajMo
+# Arquivo a ser comitado
+from palavraforca import palavra
+
+letras_usuario = []
+chances = 4
+ganhou = False
+
+while True:
+    for letra in palavra:
+        if letra.lower() in letras_usuario:
+            print(letra, end=" ")
+        else:
+            print(" _ ", end=" ")
+
+    print(f"Você tem {chances} chances!")
+
+    tentativa = input("Escolha uma letra: ")
+    letras_usuario.append(tentativa.lower())
+
+    if tentativa.lower() not in palavra.lower():
+        chances -= 1
+
+    ganhou = True
+
+    for letra in palavra:
+        if letra.lower() not in letras_usuario:
+            ganhou = False
+
+    if chances == 0 or ganhou:
+         break
+
+if ganhou:
+    # encerra o jogo
+    print(f"Parabéns! Você ganhou, a palavra era {palavra}")
+else:
+    # encerra o jogo
+    print(f"Você perdeu, a palavra era {palavra}")
---
+++ palavraforca.py
@@ -0,0 +1,9 @@
+# Sorteia uma palavra para jogar forca
+# Arquivo anexo
+# tESTE
+
+import random
+
+palavra = random.choice(["Batata", "Banana", "Tomate"])
+
+# print(palavra) Teste
